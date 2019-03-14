6c653a0c12552b7f211f0a88cde5f569152efa43
UitaStudio <edyerena086@gmail.com>
2019-03-14 10:29:31 -0600

    Initial construction of footer and header elements

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,41 @@
+<footer>
+	{{-- top footer --}}
+	<section class="footer-top wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<h6>
+						Acerca de mí
+					</h6>
+
+					<p>
+						Desarrollador web y móvil con más de 8 años de experiencia, mi objetivo es ayudar a las empresas a su crecimiento a través de la tecnología.
+					</p>
+				</div>
+
+				<div class="col-lg-6">
+					<h6>
+						Tech Partners
+					</h6>
+				</div>
+
+				<div class="col-lg-3">
+					<h6 class="text-center">
+						Información de contacto
+					</h6>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	{{-- bottom footer --}}
+	<section class="footer-bottom wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 text-center">
+					Derechos Reservados {{ \Carbon\Carbon::now()->year }}
+				</div>
+			</div>
+		</div>
+	</section>
+</footer>
---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,36 @@
+<header class="navbar navbar-expand-lg navbar-light bg-light">
+	<a class="navbar-brand" href="#">Navbar</a>
+	{{-- Mobile menu button --}}
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	{{-- navbar --}}
+	<nav class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Inicio</a>
+			</li>
+
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Servcios
+				</a>
+
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" href="#">Desarrollo de Apps Móviles</a>
+					<a class="dropdown-item" href="#">Desarrollo de Software a Medida</a>
+					<a class="dropdown-item" href="#">Diseño de páginas web</a>
+				</div>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="#">Portafolios</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="#">Contacto</a>
+			</li>
+		</ul>
+	</nav>
+</header>
